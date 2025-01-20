ece77e5fec8ee8ef4e6548be4dbadcfae6ad9f81
leeyseok <eymin123@gmail.com>
2025-01-20 09:49:09 +0000

    1212

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <button 
           onClick={() => {
-            sendGAEvent('event', 'buttonClicked', { value: '12' })
+            sendGAEvent('event', 'buttonClickasas', { value: '12' })
             sendGTMEvent({ event: 'buttonClicked', value: 'xyz' })
           }}
         >
